ea20e60220d52fe55ba1b00a0ef19dcbdd38678c
n.pervushina <n.pervushina@tinkoff.ru>
2021-12-27 09:04:43 +0500

    rewrite to FP

--- build.sbt
+++ build.sbt
@@ -10,3 +10,7 @@ libraryDependencies += "com.typesafe.akka" %% "akka-actor" % AkkaVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-remote" % AkkaVersion
 
 libraryDependencies += "com.typesafe.akka" %% "akka-serialization-jackson" % AkkaVersion
+
+libraryDependencies += "org.typelevel" %% "cats-effect" % "3.3.1"
+
+addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import Node.MyJoin
 import akka.actor.{ActorPath, ActorSystem, Props}
+import cats.effect.{IO, IOApp}
 import com.typesafe.config.{Config, ConfigFactory}
 
 class Ip(val value: String) extends AnyVal
@@ -7,53 +8,79 @@ class Ip(val value: String) extends AnyVal
 //заменить config factory на tofu config(?)
 
 object Configs {
-  private val root = ConfigFactory.load()
-  val firstSystemConfig: Config = root.getConfig("systemOne")
-  val secondSystemConfig: Config = root.getConfig("systemTwo")
+  private val root = IO.delay(ConfigFactory.load())
+  val firstSystemConfigIO: IO[Config] = root.map(_.getConfig("systemOne"))
+  val secondSystemConfigIO: IO[Config] = root.map(_.getConfig("systemOne"))
 
-  val secondSystemHost: String =
-    secondSystemConfig.getString(
-      "akka.remote.artery.canonical.hostname"
-    ) //exception
-  val secondSystemPort: String =
-    secondSystemConfig.getString(
-      "akka.remote.artery.canonical.port"
+  val secondSystemHostIO: IO[String] = {
+    secondSystemConfigIO.map(
+      _.getString(
+        "akka.remote.artery.canonical.hostname"
+      )
+    )
+  } //exception
+  val secondSystemPortIO: IO[String] =
+    secondSystemConfigIO.map(
+      _.getString(
+        "akka.remote.artery.canonical.port"
+      )
     ) //exception
 
-  val existingNodePath: ActorPath =
-    ActorPath.fromString(
-      secondSystemConfig.getString("existingNodePath")
-    ) //exception
+  val existingNodePathIO: IO[ActorPath] = {
+    secondSystemConfigIO.map(c =>
+      ActorPath.fromString(
+        c.getString("existingNodePath")
+      )
+    )
+  } //exception
 }
 
 //переписать на ФП
-object Main extends App {
+object Main extends IOApp.Simple {
   import Configs._
 
-  val existingSystem = ActorSystem("systemOne", firstSystemConfig)
-  val existingNode = existingSystem.actorOf(Props[Node], "existingNode")
-  println(s"first local path ${existingNode.path}")
+  val run = for {
+    firstSystemConfig <- firstSystemConfigIO
+    existingSystem = ActorSystem("systemOne", firstSystemConfig)
+    existingNode = existingSystem.actorOf(Props[Node], "existingNode")
+    _ <- IO.println(s"first local path ${existingNode.path}")
+    secondSystemConfig <- secondSystemConfigIO
+    newSystem = ActorSystem("systemTwo", secondSystemConfig)
+    newNode = newSystem.actorOf(Props[Node], "newNode")
+    _ <- IO.println(s"second local path ${newNode.path}")
+    existingNodePath <- existingNodePathIO
+  } yield newNode ! MyJoin(
+    existingNodePath,
+    s"$secondSystemHostIO:$secondSystemPortIO"
+  )
 
-  //We assume that the new node learns the identity of an existing Chord node by some external mechanism.
-  //Наверное нужна функция - по имени системы получаем existingNode remote path
-  //akka://systemOne/user/node
-  //akka://systemOne@127.0.0.1:2552/user/node
-
-  val newSystem = ActorSystem("systemTwo", secondSystemConfig)
-  val newNode = existingSystem.actorOf(Props[Node], "newNode")
-
-  println(s"second local path ${newNode.path}")
-
-  //sha1 test
-  val r1 = Node.sha1("127.0.0.1:2553")
-  val r2 = Node.sha1("100.0.0.0:0000")
-
-  println("sha test")
-  println(r1)
-  println(r1.bitLength) //
-  println(r2)
-  println(r2.bitLength)
-  println("sha test end")
-
-  newNode ! MyJoin(existingNodePath, s"$secondSystemHost:$secondSystemPort")
+//  val existingSystem = ActorSystem("systemOne", firstSystemConfigIO) //
+//  val existingNode = existingSystem.actorOf(Props[Node], "existingNode") //
+//  println(s"first local path ${existingNode.path}") //
+//
+//  //We assume that the new node learns the identity of an existing Chord node by some external mechanism.
+//  //Наверное нужна функция - по имени системы получаем existingNode remote path
+//  //akka://systemOne/user/node
+//  //akka://systemOne@127.0.0.1:2552/user/node
+//
+//  val newSystem = ActorSystem("systemTwo", secondSystemConfigIO) //
+//  val newNode = existingSystem.actorOf(Props[Node], "newNode") //
+//
+//  println(s"second local path ${newNode.path}")
+//
+//  //sha1 test
+////  val r1 = Node.sha1("127.0.0.1:2553")
+////  val r2 = Node.sha1("100.0.0.0:0000")
+////
+////  println("sha test")
+////  println(r1)
+////  println(r1.bitLength)
+////  println(r2)
+////  println(r2.bitLength)
+////  println("sha test end")
+//
+//  newNode ! MyJoin(
+//    existingNodePathIO,
+//    s"$secondSystemHostIO:$secondSystemPortIO"
+//  )
 }
